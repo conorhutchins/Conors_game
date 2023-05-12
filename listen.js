f9e825030d4b1fa5f3add32eb4d7f682566696a0
conorhutchins <conorhutchins@btinternet.com>
2023-05-12 13:50:28 +0100

    ready to host

--- listen.js
+++ listen.js
@@ -1,3 +1,4 @@
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
